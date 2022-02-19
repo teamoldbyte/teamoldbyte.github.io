2f04919dc2c3b04dade77dd0ff8726d7163c8c58
dongji0903 <35845323+dongji0903@users.noreply.github.com>
2022-02-19 21:00:38 +0900

    Create tutorials.js
    튜토리얼 진행 js 파일

---
+++ js/app/tutorials.js
@@ -0,0 +1,12 @@
+		$(document.body).append('<div class="modal fade" id="workbookTutorial" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">\
+			<div class="modal-dialog modal-fullscreen">\
+			<div class="modal-content fw-bold border-0">\
+			<div class="modal-header bg-fc-purple">\
+			<h5 class="modal-title text-white">워크북 등록 튜토리얼</h5>\
+			</div>\
+			<div class="modal-body text-center bg-dark">\
+			<img class="tutorial-step mh-100 mw-100" src="/images/app/tutorial/1.png">\
+			</div>\
+			</div></div></div>');
+		
+		// $('#workbookTutorial').modal('show');
