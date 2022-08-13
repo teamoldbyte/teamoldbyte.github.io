6e0a1bc94197c862852d13a9e9aa7a090b39a410
gataru-23 <95846941+gataru-23@users.noreply.github.com>
2022-08-13 10:59:28 +0900

    Add files via upload

--- js/view/mypage/view_account.js
+++ js/view/mypage/view_account.js
@@ -151,7 +151,7 @@ function pageinit(tray, normalEggCount, goldEggCount) {
 		// 골드 여부
 		const isGold = selectedEgg.matches('.gold');
 		// 골드 여부에 따라 표시여부가 달라지는 항목들 선택자
-		const selectiveInfo = Array.from(['count-text','hatching-info','writer'], s => '.egg-text-info-section .'+s).join(',');
+		const selectiveInfo = Array.from(['count-text','hatching-info'], s => '.egg-text-info-section .'+s).join(',');
 		
 		$(detail).find(selectiveInfo).toggle(!isGold);
 		
