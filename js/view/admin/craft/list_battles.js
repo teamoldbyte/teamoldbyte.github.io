b389fc12f46cbfb0cb279ade1e6f54df85bdd1d6
dongji0903 <35845323+dongji0903@users.noreply.github.com>
2022-09-12 19:41:36 +0900

    Update list_battles.js
    목록 조회를 페이지이동에서 ajax로 전환

--- js/view/admin/craft/list_battles.js
+++ js/view/admin/craft/list_battles.js
@@ -4,82 +4,15 @@
 function pageinit(battlePage) {
 	
 	const battleListContainer = document.querySelector('#battleListDiv tbody');
-	const totalPages = battlePage?.totalPages,
-		currPage = battlePage?.number + 1,
-		blockLength = 10, 
-		currBlock = Math.floor((currPage - 1) / blockLength) + 1,
-		startPage = (currBlock - 1) * blockLength + 1,
-		endPage = (startPage + blockLength <= totalPages) ? (startPage + blockLength - 1) : totalPages;
-	battleListContainer.appendChild(createElement(Array.from(battlePage.content, battle => {
-		return { el: 'tr', children: [
-			{ el: 'td', textContent: battle.rnum },
-			{ el: 'td', className: 'text-center', textContent: battle.bid },
-			{ el: 'td', className: 'text-center', textContent: battle.battleType },
-			{ el: 'td', className: 'text-center', textContent: battle.engLevel },
-			{ el: 'td', className: 'text-center', textContent: battle.grammarTitle },
-			{ el: 'td', className: 'text-start js-open-detail', style: 'width:300px', 'data-sentenceid': battle.sentenceId, onclick: function() {
-					const detailSection = document.querySelector('#battleDetailSection .ask-detail-section');
-					// 종류
-					detailSection.querySelector('.battle-type').value = battle.battleType;
-					// 질문
-					detailSection.querySelector('.battle-ask').replaceChildren(createElement(Array.from(craft.createAskOptions(battle.battleType, craft.getAsks(battle.battleType)), option => {
-						if(option.value == battle.ask) option.selected = true;
-						return option;
-					})))
-					// 계급
-					detailSection.querySelector('.battle-engLevel').value = battle.engLevel;
-					// 출처
-					detailSection.querySelector('.battle-source').value = battle.source;
-					// 본문
-					detailSection.querySelector('.battle-context').replaceChildren(createElement(craft?.createBattleContext(battle.eng, battle)));
-					// 난이도
-					detailSection.querySelector('.battle-diffLevel').value = battle.diffLevel;
-					// 문법카테고리
-					detailSection.querySelector('.battle-category').value = battle.categoryId;
-					// 태그
-					detailSection.querySelector('.battle-askTag').value = battle.askTag;
-					//코멘트
-					detailSection.querySelector('.battle-comment').value = battle.comment;
-				}, children: [
-					{ el: 'a', style: { display: 'inline-block', width: '300px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}, textContent: battle.eng }
-			]},
-			{ el: 'td', className: 'text-center', textContent: battle.askTag },
-			{ el: 'td', className: 'text-center', textContent: battle.source },
-			{ el: 'td', className: 'text-center', textContent: new Date(battle.regDate).format('yyyy-MM-dd') }
-			
-		]}
-	})));
-	
 	const battlePaginationContainer = document.querySelector('#battlePagination');
-	const pageNavs = [];
-	if(startPage > blockLength) {
-		pageNavs.push({ el: 'li', className: 'page-item', 'aria-label': 'Previous', children: [
-			{ el: 'a', className: 'page-link', 'aria-hidden': true, 'data-pagenum': startPage - 1, innerHTML: '&laquo;'}
-		]});
-	}
-	for(let pagenum = startPage; pagenum <= endPage; pagenum++) {
-		pageNavs.push({ el: 'li', className: `page-item${pagenum == currPage ? ' active':''}`, children: [
-			{ el: 'a', className: 'page-link', 'data-pagenum': pagenum, textContent: pagenum }
-		]});
-	}
-	if(endPage < totalPages) {
-		pageNavs.push({ el: 'li', className: 'page-item', 'aria-label': 'Next', children: [
-			{ el: 'a', className: 'page-link', 'aria-hidden': true, 'data-pagenum': endPage + 1, innerHTML: '&raquo;'}
-		]});		
-	}
-	battlePaginationContainer.appendChild(createElement(pageNavs));
+
+	displayBattleList(battlePage);
 	
-	// 선택한 타이틀을 제외한 나머지의 sortMark를 보이지 않도록 한다.
-	$('#battleListDiv .sortMark').hide();
-	const currentSortName = $('#searchFormHidden_list #sortName').val();
-	const $currSortMark = $(`.thlink[data-value="${currentSortName}"]+.sortMark`);
-	if($currSortMark.length > 0) {
-		$currSortMark.html($('#searchFormHidden_list #asc').val() == 'false' ? '▼' : '▲').show();
-	}
+	
 	/**
 	 * 목록 헤더 컬럼 정렬 기능 처리
 	 */
-	$('.thlink[data-value]').click(function() {
+	$(document).on('click','.thlink[data-value]', function() {
 		const sortName = this.dataset.value;
 		const $hiddenSortName = $('#searchFormHidden_list #sortName');
 		const $direction = $('#searchFormHidden_list #asc');
@@ -94,9 +27,24 @@ function pageinit(battlePage) {
 	
 	
 	// 페이지 번호를 누르면 해당 페이지로 이동
-	$('.page-link').click(function() {
+	$(document).on('click','.page-link', function() {
 		$('#searchFormHidden_list #page').val(parseInt(this.dataset.pagenum));
 		$('#pageForm').submit();
+	})
+	
+	// 새 배틀목록 조회(ajax)
+	$('#pageForm').on('submit', function(e) {
+		e.preventDefault();
+		$.ajax({
+			url: '/adminxyz/craft/battle/page',
+			data: $(this).serialize(),
+			success: function(page) {
+				displayBattleList(page);
+			}, 
+			error: function() {
+				aler('배틀 목록 조회에 실패했습니다.');
+			} 
+		});
 	})
 	
 	// 배틀 상세보기
@@ -216,5 +164,81 @@ function pageinit(battlePage) {
 		document.querySelector('#battleDetailSection .battle-category').appendChild(createElement(Array.from(categories, c => {
 			return { el:'option', value: c.cid, textContent: `${(c.parentCategory ? '└─ ':'')}${c.title}`};
 		})))
-	})
+	});
+	
+	/** 조회된 배틀을 DOM 목록 생성하여 표시하고 페이지네이션 갱신
+	 */
+	function displayBattleList(page) {
+		// 선택한 타이틀을 제외한 나머지의 sortMark를 보이지 않도록 한다.
+		$('#battleListDiv .sortMark').hide();
+		const currentSortName = $('#searchFormHidden_list #sortName').val();
+		const $currSortMark = $(`.thlink[data-value="${currentSortName}"]+.sortMark`);
+		if($currSortMark.length > 0) {
+			$currSortMark.html($('#searchFormHidden_list #asc').val() == 'false' ? '▼' : '▲').show();
+		}		
+		
+		const totalPages = page?.totalPages,
+			currPage = page?.number + 1,
+			blockLength = 10, 
+			currBlock = Math.floor((currPage - 1) / blockLength) + 1,
+			startPage = (currBlock - 1) * blockLength + 1,
+			endPage = (startPage + blockLength <= totalPages) ? (startPage + blockLength - 1) : totalPages;
+		battleListContainer.replaceChildren(createElement(Array.from(page.content, battle => {
+			return { el: 'tr', children: [
+				{ el: 'td', textContent: battle.rnum },
+				{ el: 'td', className: 'text-center', textContent: battle.bid },
+				{ el: 'td', className: 'text-center', textContent: battle.battleType },
+				{ el: 'td', className: 'text-center', textContent: battle.engLevel },
+				{ el: 'td', className: 'text-center', textContent: battle.grammarTitle },
+				{ el: 'td', className: 'text-start js-open-detail', style: 'width:300px', 'data-sentenceid': battle.sentenceId, onclick: function() {
+						const detailSection = document.querySelector('#battleDetailSection .ask-detail-section');
+						// 종류
+						detailSection.querySelector('.battle-type').value = battle.battleType;
+						// 질문
+						detailSection.querySelector('.battle-ask').replaceChildren(createElement(Array.from(craft.createAskOptions(battle.battleType, craft.getAsks(battle.battleType)), option => {
+							if(option.value == battle.ask) option.selected = true;
+							return option;
+						})))
+						// 계급
+						detailSection.querySelector('.battle-engLevel').value = battle.engLevel;
+						// 출처
+						detailSection.querySelector('.battle-source').value = battle.source;
+						// 본문
+						detailSection.querySelector('.battle-context').replaceChildren(createElement(craft?.createBattleContext(battle.eng, battle)));
+						// 난이도
+						detailSection.querySelector('.battle-diffLevel').value = battle.diffLevel;
+						// 문법카테고리
+						detailSection.querySelector('.battle-category').value = battle.categoryId;
+						// 태그
+						detailSection.querySelector('.battle-askTag').value = battle.askTag;
+						//코멘트
+						detailSection.querySelector('.battle-comment').value = battle.comment;
+					}, children: [
+						{ el: 'a', style: { display: 'inline-block', width: '300px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis'}, textContent: battle.eng }
+				]},
+				{ el: 'td', className: 'text-center', textContent: battle.askTag },
+				{ el: 'td', className: 'text-center', textContent: battle.source },
+				{ el: 'td', className: 'text-center', textContent: new Date(battle.regDate).format('yyyy-MM-dd') }
+				
+			]}
+		})));	
+		
+		const pageNavs = [];
+		if(startPage > blockLength) {
+			pageNavs.push({ el: 'li', className: 'page-item', 'aria-label': 'Previous', children: [
+				{ el: 'a', className: 'page-link', 'aria-hidden': true, 'data-pagenum': startPage - 1, innerHTML: '&laquo;'}
+			]});
+		}
+		for(let pagenum = startPage; pagenum <= endPage; pagenum++) {
+			pageNavs.push({ el: 'li', className: `page-item${pagenum == currPage ? ' active':''}`, children: [
+				{ el: 'a', className: 'page-link', 'data-pagenum': pagenum, textContent: pagenum }
+			]});
+		}
+		if(endPage < totalPages) {
+			pageNavs.push({ el: 'li', className: 'page-item', 'aria-label': 'Next', children: [
+				{ el: 'a', className: 'page-link', 'aria-hidden': true, 'data-pagenum': endPage + 1, innerHTML: '&raquo;'}
+			]});		
+		}
+		battlePaginationContainer.replaceChildren(createElement(pageNavs));			
+	}
 }
