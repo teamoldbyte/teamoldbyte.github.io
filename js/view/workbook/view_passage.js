d3cce7ccf1ce6bf2a17d65d3c214587db6967fa9
teamoldbyte <96049059+teamoldbyte@users.noreply.github.com>
2023-06-04 18:15:18 +0900

    Update view_passage.js

--- js/view/workbook/view_passage.js
+++ js/view/workbook/view_passage.js
@@ -1283,7 +1283,7 @@
 	// 인덱스핑거의 유용성 false로 지정
 	.on('click', '.js-set-worthless', function() {
 		const sentenceId = $(this).closest('.js-finger-detail').data('sentenceId');
-		confirmModal('<div class="text-start row"><span class="col-12 text-center">학습에 도움되지 않는 문장으로 보고합니다.<br>다음과 같은 사유가 해당됩니다.</span><div class="mx-auto w-auto d-block"><br>	• 오타가 포함된 문장<br>	• 불완전하게 잘린 문장<br>	• 무의미한 문자로 나열된 문장<br>	• 기타 비문장</div></div>', () => {
+		confirmModal('<div class="text-start row"><span class="col-12 text-center">완전한 문장의 구성으로 충족되지 않습니다.<br>다음과 같은 경우에 해당합니다.</span><div class="mx-auto w-auto d-block"><br>	• 오타가 포함된 문장<br>	• 불완전하게 잘린 문장<br>	• 문장구성이 안되는 문장<br>	• 불완전한 인용구, 구두점오류가 있는 문장<br>	• 기타 비문장</div></div>', () => {
 			$.ajax({
 				url: '/adminxyz/sentence/useful/edit',
 				type: 'POST',
