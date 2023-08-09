84fa63440271079a9fa47831d273917a4ad75ebd
teamoldbyte <96049059+teamoldbyte@users.noreply.github.com>
2023-08-09 15:31:53 +0900

    Update add_sentences.js
    이예선씨 하루제한 예외 적용

--- js/view/workbook/add_sentences.js
+++ js/view/workbook/add_sentences.js
@@ -31,7 +31,7 @@ function pageinit(memberId) {
 	}
 	
 	function _verifyUsageLimit() {
-		if(myFicoUsages.length >= MAX_SENTENCE_LENGTH_PER_DAY) {
+		if(memberId != 15000550 && myFicoUsages.length >= MAX_SENTENCE_LENGTH_PER_DAY) {
 			$('#inputComplete, .ocr-btn').prop('disabled', true);
 			$('#newPassageText').prop('disabled', true).addClass('form-control').attr('placeholder', `일일 분석량(${MAX_SENTENCE_LENGTH_PER_DAY_STR}자)을 모두 소진했습니다. 내일 다시 찾아와 주세요.`);
 			alertModal(`일일 분석량<span class="text-red-700">(${MAX_SENTENCE_LENGTH_PER_DAY_STR}자)</span>을 모두 <span class="text-red-700">소진</span>했습니다.\n내일 다시 찾아와 주세요.`);
