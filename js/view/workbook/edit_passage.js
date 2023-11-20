904732c8a89053c7eb1139600b9903f8ab3d0b46
teamoldbyte <96049059+teamoldbyte@users.noreply.github.com>
2023-11-20 12:03:54 +0900

    Update edit_passage.js
    referrer로 workdId56을 못받을 경우
    sessionStorage도 뒤져서 찾도록.

--- js/view/workbook/edit_passage.js
+++ js/view/workbook/edit_passage.js
@@ -7,6 +7,8 @@ function pageinit(sentenceList, memberId, isSsam) {
 	let workbookId56;
 	if(document.referrer) {
 		workbookId56 = new URL(document.referrer).pathname?.match(/(\/mybook\/edit\/|\/workbook\/study\/overview\/|\/workbook\/passage\/)(\w+)/)?.[2];
+	}else if(sessionStorage.getItem('workbookId')) {
+		workbookId56 = ntoa(sessionStorage.getItem('workbookId'));
 	}
 	const LIST_SENTENCE_SELECTOR = '.list-sentence-section',
 		ONE_SENTENCE_SELECTOR = '.one-sentence-unit-section';
