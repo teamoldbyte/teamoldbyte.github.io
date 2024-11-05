647c738172b7244dc5312514520d2df2428538b8
teamoldbyte <96049059+teamoldbyte@users.noreply.github.com>
2024-11-05 14:56:24 +0900

    Update add_sentences.js
    검색 결과 지문 속 연속된 문장이 similar 타입인 경우 태그를 분리

--- js/view/workbook/add_sentences.js
+++ js/view/workbook/add_sentences.js
@@ -466,7 +466,7 @@ function pageinit(memberId, isSsam) {
 								break;
 						}
 						if(j > 0) {
-							if(summary[j - 1].matchType != sentence.matchType)
+							if(summary[j - 1].matchType != sentence.matchType || sentence.matchType == 'similar')
 								htmlText += '</span> <span ' + style + '>';
 							else htmlText += ' ';
 						}else htmlText += style + '>';
