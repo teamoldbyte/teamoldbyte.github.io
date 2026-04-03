bdee36f7ffcb47a95a42ede97b67bc3113355605
teamoldbyte <96049059+teamoldbyte@users.noreply.github.com>
2026-04-03 15:29:39 +0900

    Update summernote.plugins.js

--- js/util/summernote.plugins.js
+++ js/util/summernote.plugins.js
@@ -14,9 +14,9 @@
         factory(window.jQuery);
     }
 }(function ($) {
-	$.extend($.summernote.options, {
+	/*$.extend($.summernote.options, {
 		enterHtml: '<br>' // 기본 개행 태그를 <br>로 설정
-	});
+	});*/
 	// use <br> instead of <p> when 'Enter' pressed.
 	$.extend($.summernote.plugins, {
 		'extraButtons': function(context){
@@ -45,54 +45,8 @@
 				return dropdownbutton;
 			})
 		},
-        'brenter': function () {
+        'customEvents': function () {
             this.events = {
-				// Bind on ENTER
-				'summernote.enter': function (_we, e) {
-					getSelection().getRangeAt(0).deleteContents();
-					const range = getSelection().getRangeAt(0);
-					const {endContainer, endOffset} = range;
-					e.preventDefault();
-					if(
-						endContainer.nodeType == Node.TEXT_NODE 
-					// 선택 범위가 텍스트 노드이면서
-						&& (
-							endOffset < endContainer.textContent.length
-							// 텍스트 한 중간이거나, 바로 앞이나 뒤의 노드가 BR이면
-							|| !!endContainer.nextSibling && endContainer.nextSibling?.nodeType != Node.TEXT_NODE
-							|| !!endContainer.parentNode?.nextSibling && endContainer.parentNode?.nextSibling?.nodeType != Node.TEXT_NODE
-						)
-					|| endContainer.nodeType != Node.TEXT_NODE
-						&& (
-							endContainer.nextSibling?.nodeName == 'BR' 
-						)
-					) {
-						const $br = $('<br>')
-						// First, insert <br>.
-						range.insertNode($br[0]);
-						// Second, remove all other ranges.
-						const sel = getSelection();
-						sel.removeAllRanges();
-						// Third, create a range of <br>, move cursor end of it.
-						const brRange = new Range();
-						brRange.setStartAfter($br[0]);
-						sel.addRange(brRange);
-						sel.collapseToEnd();
-					}else {
-						const $br = $('<br>')
-						// First, insert <br>.
-						range.insertNode($br[0]);
-						range.insertNode($br[0]);
-						// Second, remove all other ranges.
-						const sel = getSelection();
-						sel.removeAllRanges();
-						// Third, create a range of <br>, move cursor end of it.
-						const brRange = new Range();
-						brRange.setStartAfter($br[0]);
-						sel.addRange(brRange);
-						sel.collapseToEnd();
-					}
-                },
                 // 탭 키 입력 시 고정길이 공백 입력이 아닌 진짜 탭 문자 적용(css로 white-space:break-spaces;를 적용해야 함.) 
                 'summernote.keydown': function(_we,e) {
 					if(e.code == 'Tab' && !e.shiftKey) {
