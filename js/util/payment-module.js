b13d31cc6d8536664792706ebf9cff3f2ee6edc2
teamoldbyte <96049059+teamoldbyte@users.noreply.github.com>
2026-07-20 16:58:18 +0900

    Update payment-module.js
    정기결제(테스트) 추가

--- js/util/payment-module.js
+++ js/util/payment-module.js
@@ -2,15 +2,25 @@
 const FicoPaymentHandler = (() => {
 	const CLIENT_KEY = 'live_gck_P9BRQmyarYxp6gP1xd07VJ07KzLN';
 	const allowedRequestPaymentKeys = [
-		"orderId", "orderName", "successUrl", "failUrl", "customerName", "customerEmail", "customerMobilePhone"
+		"method", "orderId", "orderName", "successUrl", "failUrl", "customerName", "customerEmail", "customerMobilePhone"
 	]
 	let _widgetOptions = {
 		successUrl: window.location.origin + "/payment/request/success", // 결제 요청이 성공하면 리다이렉트되는 URL
 		failUrl: window.location.origin + "/payment/request/fail", // 결제 요청이 실패하면 리다이렉트되는 URL,
-	}, _tossPayments, _widgets, _paymentMethodWidget;
+	}, 
+	_billAuthOptions = {
+		method: 'CARD',
+		successUrl: window.location.origin + "/billing/request/success", // 결제 요청이 성공하면 리다이렉트되는 URL
+		failUrl: window.location.origin + "/billing/request/fail", // 결제 요청이 실패하면 리다이렉트되는 URL,
+	},
+	_tossPayments, _tossBillingPayments, _billingPayments, _widgets, _paymentMethodWidget;
 	
 	const paymentSDKPromise = $.getScript('https://js.tosspayments.com/v2/standard', () => {
 		_tossPayments = TossPayments(CLIENT_KEY);
+	});
+	
+	const billingSDKPromise = $.getScript('https://js.tosspayments.com/v2/standard', () => {
+		_tossBillingPayments = TossPayments("test_ck_6BYq7GWPVveEgElQbqxa3NE5vbo1");
 	});
 
 	/**
@@ -27,7 +37,7 @@ const FicoPaymentHandler = (() => {
 		await _widgets.setAmount({
 			currency: 'KRW', value: amount
 		});
-		_paymentMethodWidget = await _widgets.renderPaymentMethods({ selector });
+		return _widgets.renderPaymentMethods({ selector }).then((widget) => _paymentMethodWidget = widget);
 	}
 	/**
 	 * 결제 요청
@@ -40,6 +50,30 @@ const FicoPaymentHandler = (() => {
 	} 
 	
 	/**
+	 * 카드 등록창 렌더링
+	 */
+	const requestBillingAuth = async (renderOptions, extParams = null) => {
+		await billingSDKPromise;
+		Object.assign(_billAuthOptions, renderOptions);
+		const { customerKey } = renderOptions;
+		_billingPayments = _tossBillingPayments.payment({ customerKey });
+		
+		
+		const params = Object.fromEntries(Object.entries(_billAuthOptions).filter(([key, value]) => 
+			allowedRequestPaymentKeys.includes(key) && value !== undefined
+		));
+		if(extParams != null) {
+			const successUrl = new URL(params.successUrl);
+			const searchParams = successUrl.searchParams;
+			Object.keys(extParams).forEach((key) => {
+				searchParams.append(key, extParams[key]);
+			});
+			params.successUrl = successUrl.toString();
+		}
+		return _billingPayments.requestBillingAuth(params);
+	}
+	
+	/**
 	 * 결제 위젯 리셋
 	 */
 	const destroy = () => {
@@ -49,6 +83,7 @@ const FicoPaymentHandler = (() => {
 	return {
 		renderWidget,
 		requestPayment,
+		requestBillingAuth,
 		destroy
 	}
 })();
